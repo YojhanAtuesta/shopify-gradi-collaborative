01933456e7418c051ccd6a8ca422ac3dd0192dc7
Miguel Angel Camacho <miguel.camacho@gradiweb.com>
2022-02-09 12:04:09 -0500

    sessionStorage

--- assets/favoriteproduct.js
+++ assets/favoriteproduct.js
@@ -1,5 +1,28 @@
 let button = document.querySelectorAll(".favourite__button")
+let flag = false 
 
 function saveFavoriteProducts(object) {
+    let dataFavorites = []
+
+    if(!flag){
+        let productFavorite = object.getAttribute("product")
+        let usefulData = JSON.parse(productFavorite)
+        dataFavorites.push(usefulData)
+        let usefulDataFavorites = JSON.stringify(dataFavorites)
+        sessionStorage.setItem("data", usefulDataFavorites);
+        flag = true
+
+    }  else{
+        let productFavorite = object.getAttribute("product")
+        let usefulData = JSON.parse(productFavorite)
+        let dataFavorites = sessionStorage.getItem("data")
+        let parseuldataFavorites = JSON.parse(dataFavorites)
+        parseuldataFavorites.push(usefulData)
+        let usefulDataFavorites = JSON.stringify(parseuldataFavorites)
+        sessionStorage.setItem("data", usefulDataFavorites);
+    }
+
+
+    
 
 }
