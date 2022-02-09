dc372c4670d67df305b89aebf9055989c396176e
HidenPlus <aatuesta00@hotmail.com>
2022-02-09 13:20:20 -0500

    Remove element

--- assets/favoriteproduct.js
+++ assets/favoriteproduct.js
@@ -1,28 +1,49 @@
-let button = document.querySelectorAll(".favourite__button")
-let flag = false 
+
+function removeItem(object) {
+    let dataFavorites = sessionStorage.getItem("data");
+    dataFavorites  = JSON.parse(dataFavorites);
+    let productFavorite = object.getAttribute("product");
+    let usefulData = JSON.parse(productFavorite);
+    console.log(dataFavorites, usefulData)
+    dataFavorites = dataFavorites.filter((data) => data.id != usefulData.id);
+    console.log(dataFavorites);
+    sessionStorage.setItem("data", JSON.stringify(dataFavorites));
+    verifyItemInStorage();
+}
 
 function saveFavoriteProducts(object) {
-    let dataFavorites = []
-
-    if(!flag){
-        let productFavorite = object.getAttribute("product")
-        let usefulData = JSON.parse(productFavorite)
-        dataFavorites.push(usefulData)
-        let usefulDataFavorites = JSON.stringify(dataFavorites)
-        sessionStorage.setItem("data", usefulDataFavorites);
-        flag = true
-
-    }  else{
-        let productFavorite = object.getAttribute("product")
-        let usefulData = JSON.parse(productFavorite)
-        let dataFavorites = sessionStorage.getItem("data")
-        let parseuldataFavorites = JSON.parse(dataFavorites)
-        parseuldataFavorites.push(usefulData)
-        let usefulDataFavorites = JSON.stringify(parseuldataFavorites)
-        sessionStorage.setItem("data", usefulDataFavorites);
-    }
-
-
-    
-
+    let dataFavorites = [];
+    let productFavorite = object.getAttribute("product");
+    let usefulData = JSON.parse(productFavorite);
+    dataFavorites = sessionStorage.getItem("data") || [];
+    let parseuldataFavorites = JSON.parse(dataFavorites);
+    parseuldataFavorites.push(usefulData);
+    let usefulDataFavorites = JSON.stringify(parseuldataFavorites);
+    sessionStorage.setItem("data", usefulDataFavorites);
+    verifyItemInStorage();
 }
+
+function verifyItemInStorage(){
+    let dataFavorites = sessionStorage.getItem("data");
+    dataFavorites  = JSON.parse(dataFavorites);
+    let favoritesIds = dataFavorites.map(function(item){
+        return item.id;
+    });
+    let buttons = document.getElementsByClassName("favourite__button");
+    buttons = Array.from(buttons);
+    let product;
+
+    buttons.forEach((button) => {
+      product = button.getAttribute("product");
+      let usefulData = JSON.parse(product);
+      if(favoritesIds.includes(usefulData.id)){
+        button.setAttribute("onclick", "removeItem(this)")
+        button.innerHTML = "Eliminar de favoritos"
+      }else{
+        button.setAttribute("onclick", "saveFavoriteProducts(this)")
+        button.innerHTML = "Agregar a favoritos";
+      }
+    });
+}
+
+window.addEventListener("load", verifyItemInStorage);
